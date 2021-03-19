3acc2537bd841498a259dc7ade3b148b06dc83e1
Muktar2298 <muktar15-2298@diu.edu.bd>
2021-03-19 16:12:36 +0600

    User-Router-Practice

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,22 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './User.css';
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
+import "./User.css";
 
 const User = (props) => {
-    // console.log(props.user)
-    const {name,email,id} = props.user;
-    return (
-        <div className="user-container">
-            <h5>Name : {name}</h5>
-            <p>Email : {email}</p>
-            <Link to={'/user/' + id}><button className="main-btn">Show User Details {id}</button></Link>
-            {/* --Dynamic using Template string(ES6)--
+  // console.log(props.user)
+  const { name, email, id } = props.user;
+
+  // --handleBtn--//
+  let history = useHistory();
+  const handleBtn = () => {
+    history.push(`/user/${id}`);
+  };
+  return (
+    <div className="user-container">
+      <h5>Name : {name}</h5>
+      <p>Email : {email}</p>
+      <Link to={"/user/" + id}>
+        <button className="main-btn">Show User Details {id}</button>
+      </Link>
+      {/* --Dynamic using Template string(ES6)--
             <Link to={`/user/ ${id}`}> Show User Details</Link> */}
-          <br/>
-            {/* --using eventHandeler for navigate -- */}
-            <button>Show Details {id}</button>
-        </div>
-    );
+      <br />
+
+      {/* --using eventHandeler for navigate -- */}
+      <button onClick={handleBtn}>Show Details {id}</button>
+    </div>
+  );
 };
 
 export default User;
